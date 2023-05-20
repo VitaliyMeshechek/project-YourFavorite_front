0b090bc4ee5bf1f5cca946e42267c9feea758d59
Ihor <ihorrykun@gmail.com>
2023-05-20 11:18:49 +0300

    fix update mainPage

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,14 +1,9 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Container, Input, Title } from './MainPage.styled';
 
 const MainPage = () => {
   return (
     <>
-      <Container>
-        <Title> Find your favorite pet</Title>
-        <Input placeholder="search"></Input>
-      </Container>
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
