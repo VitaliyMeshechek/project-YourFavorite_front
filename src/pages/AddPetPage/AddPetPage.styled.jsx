aab2e695a4643a9fb6a7bdd109e6766d3058f51f
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-05-22 01:53:46 +0300

    addpet

---
+++ src/pages/AddPetPage/AddPetPage.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const AddFormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 8px 17px;
+  width: 100%;
+  min-height: 496px;
+  background: #FDF7F2;
+  box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
+  border-radius: 40px;
+  }
+`;
