05372a6112e857b2e18670eddc5df4a94a9c8845
Oleg-Strong <superstrong0@gmail.com>
2023-05-24 14:47:32 +0300

    add Header

--- src/components/Header/UserNav/UserNav.jsx
+++ src/components/Header/UserNav/UserNav.jsx
@@ -1,13 +1,30 @@
-import { NavLink } from 'react-router-dom';
+import { useAuth } from 'hooks';
+import {
+  UserWrapper,
+  UserLink,
+  Avatar,
+  UserName,
+  AvatarLink,
+} from './UserNav.styled';
 
 export const UserNav = () => {
-  return (
-      <>
-        
-          <NavLink to='/user' key='/user'>
-            User
-          </NavLink>
+  const { user } = useAuth();
+  const defaultName = user.email ? user.email.split('@')[0] : 'User';
 
-      </>
+  return (
+    <UserWrapper>
+      <UserLink to="/user">
+        <Avatar />
+        <UserName>{user.name ? user.name : defaultName}</UserName>
+      </UserLink>
+    </UserWrapper>
+  );
+};
+
+export const NavAvatar = () => {
+  return (
+    <AvatarLink to="/user">
+      <Avatar />
+    </AvatarLink>
   );
 };
