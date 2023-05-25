7d50b467b1b79a699bf373e3519c7b6a2e3b3244
VitaliyMeshechek <meshechek.vitaliy@ukr.net>
2023-05-25 17:12:56 +0300

    add import

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,5 +1,5 @@
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+// import { Suspense } from 'react';
+// import { Outlet } from 'react-router-dom';
 import { FirstMainPage } from '../../components/MainContent/MainContent';
 const MainPage = () => {
   return <FirstMainPage />;
