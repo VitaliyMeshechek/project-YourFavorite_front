1d0503f00e2fcd1144ee66e240542335af052371
VitaliyMeshechek <108028369+VitaliyMeshechek@users.noreply.github.com>
2023-06-02 12:21:04 +0000

    Deploying to gh-pages from @ VitaliyMeshechek/project-YourFavorite_front@ceb9350aa8fb49c8cf84664b486a308402b8c2c3 🚀

---
+++ static/js/820.062c2aa1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[820],{8820:function(t,e,c){c.d(e,{bzc:function(){return r}});var a=c(9983);function r(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 0 0-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"}}]})(t)}}}]);
+//# sourceMappingURL=820.062c2aa1.chunk.js.map
